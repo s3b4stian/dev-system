970b77a1c64b0821415cb0e3dc15c994abe26924
s3b4stian <sebastian.rapetti@tim.it>
2020-03-17 11:14:58 +0100

    dev updates

--- php_tools.sh
+++ php_tools.sh
@@ -6,6 +6,7 @@ USER=sebastian
 mkdir /home/$USER/html/bin
 rm /home/$USER/html/bin/*
 
+
 ## Download PHP tools
 
 ### PHP cs Fixer
@@ -20,9 +21,25 @@ wget https://phar.phpunit.de/phpunit.phar -P /home/$USER/html/bin
 ### Infection
 wget https://github.com/infection/infection/releases/download/0.15.3/infection.phar -P /home/$USER/html/bin
 
+
+## Make all executable
+cd /home/$USER/html/bin
+
+mv php-cs-fixer.phar php-cs-fixer
+mv composer-stable.phar composer
+mv phpunit.phar phpunit
+mv infection.phar infection
+
+chmod +x php-cs-fixer
+chmod +x composer
+chmod +x phpunit
+chmod +x infection
+
+
 ## Copy update script
 cp ./php/update-all-tools.sh /home/$USER/html/bin/update-all-tools
 chmod +x /home/$USER/html/bin/update-all-tools
+
 
 ## Copy all to sbin
 sudo cp /home/$USER/html/bin/* /usr/local/sbin
