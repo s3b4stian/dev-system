d1977561d4b867a5b60a33b0cedd1d00e4141714
s3b4stian <sebastian.rapetti@tim.it>
2021-12-01 19:58:52 +0000

    all tools updated

--- 50-php_tools.sh
+++ 50-php_tools.sh
@@ -2,8 +2,8 @@
 
 source config.sh
 
-VER_CS_FIXER=v2.18.4
-VER_INFECTION=0.21.4
+VER_CS_FIXER=v3.3.2
+VER_INFECTION=0.25.3
 
 ## Create directory or clean if already present
 ## --------------------------------------------
