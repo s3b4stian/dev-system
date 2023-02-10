3f6293aeda2a39a8327181a101536972dc17a16c
s3b4stian <sebastian.rapetti@tim.it>
2023-02-10 18:33:08 +0000

    docker install update

--- 10-prepare.sh
+++ 10-prepare.sh
@@ -13,9 +13,23 @@ sudo apt-get -y upgrade
 
 ## Installing Docker and docker-compose
 ## ------------------------------------
-printf  "\n${RED}Installing Docker and docker-compose${NC}\n"
+printf  "\n${RED}Installing docker and docker-compose${NC}\n"
 
-sudo apt-get -y install docker docker.io docker-compose
+dpkg -l | grep docker | awk '{print $2}' | sudo xargs apt -y remove
+dpkg -l | grep '^rc' | awk '{print $2}' | sudo xargs dpkg --purge
+
+# Get official docker key
+sudo mkdir -m 0755 -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+# Insert official docker repo
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Install
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-compose
+
 printf "${YELLOW}";docker --version;printf "${NC}\n"
 
 
