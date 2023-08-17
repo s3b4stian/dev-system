8f89a551a13a3b5d285ea8ac47a757c390665b5e
Sebastian Rapetti <sebastian.rapetti@tim.it>
2023-08-17 22:07:22 +0200

    latest version of infection and php-cs-fixer

--- 50-php_tools.sh
+++ 50-php_tools.sh
@@ -2,8 +2,8 @@
 
 source config.sh
 
-VER_CS_FIXER=v3.15.1
-VER_INFECTION=0.26.19
+#VER_CS_FIXER=v3.23.0
+#VER_INFECTION=0.27.0
 
 ## Create directory or clean if already present
 ## --------------------------------------------
@@ -18,15 +18,17 @@ rm /home/$USER/html/bin/*
 printf "${RED}Download PHP tools${NC}\n"
 
 ### PHP cs Fixer
-wget https://github.com/FriendsOfPHP/PHP-CS-Fixer/releases/download/$VER_CS_FIXER/php-cs-fixer.phar -P /home/$USER/html/bin
+#wget https://github.com/FriendsOfPHP/PHP-CS-Fixer/releases/download/$VER_CS_FIXER/php-cs-fixer.phar -P /home/$USER/html/bin
+curl -sL https://api.github.com/repos/FriendsOfPHP/PHP-CS-Fixer/releases/latest | grep "php-cs-fixer.phar" | cut -d : -f 2,3 | tr -d \" | head -n 2 | tail -n 1 | wget -i - -P /home/$USER/html/bin
 ### Composer
 wget https://getcomposer.org/composer-stable.phar -P /home/$USER/html/bin
 ### PHPunit
 wget https://phar.phpunit.de/phpunit.phar -P /home/$USER/html/bin
 ### Infection
-wget https://github.com/infection/infection/releases/download/$VER_INFECTION/infection.phar -P /home/$USER/html/bin
+#wget https://github.com/infection/infection/releases/download/$VER_INFECTION/infection.phar -P /home/$USER/html/bin
+curl -sL https://api.github.com/repos/infection/infection/releases/latest | grep "infection.phar" | cut -d : -f 2,3 | tr -d \" | head -n 2 | tail -n 1 | wget -i - -P /home/$USER/html/bin
 ### PHP Documentor
-wget wget https://phpdoc.org/phpDocumentor.phar -P /home/$USER/html/bin
+wget https://phpdoc.org/phpDocumentor.phar -P /home/$USER/html/bin
 
 
 ## Make all executable
@@ -39,7 +41,7 @@ mv php-cs-fixer.phar php-cs-fixer
 mv composer-stable.phar composer
 mv phpunit.phar phpunit
 mv infection.phar infection
-mv phpDocumentor.phar php-doc
+mv phpDocumentor.phar php-doc.phar
 
 chmod +x php-cs-fixer
 chmod +x composer
@@ -63,6 +65,9 @@ chmod +x /home/$USER/html/bin/cs-fix-all
 printf "${RED}Copy all to sbin${NC}\n"
 
 sudo cp /home/$USER/html/bin/* /usr/local/sbin
+
+# make link to permit the compressed phar to work
+sudo ln -s /usr/local/sbin/php-doc.phar php-doc
 
 php-cs-fixer --version
 composer --version
